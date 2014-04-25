185179ddf7876ad1d67a8b59039712a1af357b9d
alextbok <alexander_bok@brown.edu>
2014-04-25 18:54:34 -0400

    Big push. Basically optimized repainting the sound panel, so it should be a lot less laggy (especially when turning the knobs).

---
+++ src/frontend/soundpanel/KnobPanel.java
@@ -0,0 +1,158 @@
+package frontend.soundpanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+import backend.audio.SongApp;
+
+/**
+ * KnobPanel class
+ * JPanel that contains a JLabel with text relevant to the instance's function (e.g. "Volume")
+ * The knob icon will be painted inside the panel
+ * @author abok
+ *
+ */
+@SuppressWarnings("serial")
+public class KnobPanel extends JPanel {
+
+	/*Dimension of picture - not equal to dimension of panel (hard-coded because we cropped the picture file to 75x75 pixels)*/
+	public static final int WIDTH = 75;
+	public static final int HEIGHT = 75;
+	
+	/*Current angle of image in degrees*/
+	private double _angle;
+	
+	/*Title text (indicative of instance's function) associated with this knob*/
+	private String _text;
+	
+	/*Our knob icon, accessed via SongPanel*/
+	private static final Image icon = SongPanel.getIcon("knob");
+	
+	/*Mouse click y coordinate used on MouseDragged events*/
+	private int _previousY = 0;
+	
+	/**
+	 * Constructor
+	 */
+	public KnobPanel(String s) {
+		_angle = 112.0;
+		_text = s;
+	   	
+		//create border and its title font
+		TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(192,193,194), 2, true), _text);
+		border.setTitleFont(new Font("Courier", Font.BOLD, 12));
+		border.setTitleColor(new Color(135,136,138));
+	   	this.setBorder(border);
+	   	
+	   	this.addMouseListener(new KnobMouseListener());
+	   	this.addMouseMotionListener(new KnobMouseMotionListener());
+	   	
+	   	//tweak size and look of panel
+		this.setLayout(new BorderLayout());
+		this.setPreferredSize(new Dimension(WIDTH + 15,HEIGHT + 15));
+		this.setMaximumSize(new Dimension(WIDTH + 15,HEIGHT + 15));
+		this.setBackground(SongPanel.BACKGROUND_COLOR);
+		this.setVisible(true);
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		
+        super.paintComponent(g);
+        Graphics2D brush = (Graphics2D) g;
+        
+        //rotate brush around volume knob by its current angle before painting it
+        double angleVol = Math.toRadians(_angle);
+        brush.rotate(angleVol, KnobPanel.WIDTH/2 + SongPanel.KNOB_X_OFFSET, KnobPanel.HEIGHT/2 + SongPanel.KNOB_Y_OFFSET);
+        brush.drawImage(icon, SongPanel.KNOB_X_OFFSET, SongPanel.KNOB_Y_OFFSET, null);
+        //rotate brush back
+        brush.rotate(-angleVol, KnobPanel.WIDTH/2 + SongPanel.KNOB_X_OFFSET, KnobPanel.HEIGHT/2 + SongPanel.KNOB_Y_OFFSET);
+	}
+	
+	/*ACCESSORS*/
+
+	/**
+	 * Increases the angle by 5.0 degrees
+	 * The double will be converted to radians before repainting
+	 */
+	private void rotateImage(double d) {
+		_angle += d;
+	     if (_angle >= 360.0)
+	       _angle = 0.0;
+	}
+	
+    /**
+     * 
+     * Private class KnobMouseListener that listens for mouse down events
+     * We store the y-coordinate on this event, which will be used on the
+     * MouseDragged event in our mouse motion listener
+     *
+     */
+	private class KnobMouseListener implements MouseListener {
+
+		public KnobMouseListener() {
+
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			_previousY = e.getY();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+	}
+	
+	
+	/**
+	 * Private class that listens for MouseDragged events, which turn the knobs
+	 * @author abok
+	 *
+	 */
+	private class KnobMouseMotionListener implements MouseMotionListener {
+
+		public KnobMouseMotionListener() {
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			double rotation = 0.5 * (_previousY - e.getY());
+			KnobPanel.this.rotateImage(rotation);
+
+			if (_text.equals("Volume"))
+				SongApp.changeVolume(rotation / 50);
+			else if (_text.equals("Speed"))
+				SongApp.changeSpeed(rotation * 0.001);
+
+			_previousY = e.getY();
+			KnobPanel.this.repaint();
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {}
+	}
+	
+	
+}
--- src/frontend/soundpanel/SongPanel.java
+++ src/frontend/soundpanel/SongPanel.java
@@ -7,15 +7,11 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
 import frontend.GUI;
 import backend.audio.SongApp;
 
@@ -29,24 +25,27 @@ public class SongPanel extends JPanel {
 	/*Store this in a variable so our Knobs can access same color*/
 	public static final Color BACKGROUND_COLOR = new Color(214,212,210);
 	
-	/*Volume control knob*/
-	public static final Knob knobVolume = new Knob("Volume");
+	/*Volume control knob panel*/
+	public static final KnobPanel knobVolume = new KnobPanel("Volume");
 	
-	/*Speed control knob*/
-	public static final Knob knobSpeed = new Knob("Speed");
+	/*Speed control knob panel*/
+	public static final KnobPanel knobSpeed = new KnobPanel("Speed");
 	
-	/*Frequency control knobs*/
-	public static final Knob knobLow = new Knob("Low");
-	public static final Knob knobMid = new Knob("Mid");
-	public static final Knob knobHigh = new Knob("High");
+	/*Frequency control knob panels*/
+	public static final KnobPanel knobLow = new KnobPanel("Low");
+	public static final KnobPanel knobMid = new KnobPanel("Mid");
+	public static final KnobPanel knobHigh = new KnobPanel("High");
 	
+	/*Our icons to paint*/
+	private static final Image playIcon = getIcon("play");
+	private static final Image pauseIcon = getIcon("pause");
+	private static final Image addIcon = getIcon("add");
+	private static final Image removeIcon = getIcon("remove");
 	
-	/*Keeps track of mouse-down y-coordinates for the MouseDraggedEvent*/
-	private HashMap<String, Integer> _mouseDownCoordinates;
 	
 	/*Constants that help keep track of where to paint things*/
-	private static final int KNOB_X_OFFSET = 7;
-	private static final int KNOB_Y_OFFSET = 10;
+	public static final int KNOB_X_OFFSET = 7;
+	public static final int KNOB_Y_OFFSET = 10;
 	private static final int BTN_SIZE = 40;
 	private static final int PLAY_X = 990;
 	private static final int PLAY_Y = 10;
@@ -65,12 +64,8 @@ public class SongPanel extends JPanel {
 	 */
 	public SongPanel() {
 
-		//add our knobs and make their map
-		_mouseDownCoordinates = new HashMap<String, Integer>();
-		
 		this.add(SongList.getScrollableList());
-		this.addKnobs();
-		
+		this.addKnobs();	
 		
 		//add our button mouse listener
 		this.addMouseListener(new SoundPanelMouseListener());
@@ -90,79 +85,23 @@ public class SongPanel extends JPanel {
         Graphics2D brush = (Graphics2D) g;
         
         //draw buttons
-        brush.drawImage(getIcon("play"), PLAY_X, PLAY_Y, null);
-        brush.drawImage(getIcon("pause"), PAUSE_X, PAUSE_Y, null);
-        brush.drawImage(getIcon("add"), ADD_X, ADD_Y, null);
-        brush.drawImage(getIcon("remove"), REMOVE_X, REMOVE_Y, null);
-        
-        //rotate brush around volume knob by its current angle before painting it
-        double angleVol = Math.toRadians(knobVolume.getAngle());
-        brush.rotate(angleVol, Knob.WIDTH/2 + knobVolume.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobVolume.getY() + KNOB_Y_OFFSET);
-        brush.drawImage(Knob.getImage(), knobVolume.getX() + KNOB_X_OFFSET, knobVolume.getY() + KNOB_Y_OFFSET, null);
-        //rotate brush back
-        brush.rotate(-angleVol, Knob.WIDTH/2 + knobVolume.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobVolume.getY() + KNOB_Y_OFFSET);
-    
-        //rotate brush around speed knob by its current angle before painting it        
-        double angleSpeed = Math.toRadians(knobSpeed.getAngle());
-        brush.rotate(angleSpeed, Knob.WIDTH/2 + knobSpeed.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobSpeed.getY() + KNOB_Y_OFFSET);
-        brush.drawImage(Knob.getImage(), knobSpeed.getX() + KNOB_X_OFFSET, knobSpeed.getY() + KNOB_Y_OFFSET, null);
-        //rotate brush back
-        brush.rotate(-angleSpeed, Knob.WIDTH/2 + knobSpeed.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobSpeed.getY() + KNOB_Y_OFFSET);
-        
-        //rotate brush around low knob by its current angle before painting it        
-        double angleLow = Math.toRadians(knobLow.getAngle());
-        brush.rotate(angleLow, Knob.WIDTH/2 + knobLow.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobLow.getY() + KNOB_Y_OFFSET);
-        brush.drawImage(Knob.getImage(), knobLow.getX() + KNOB_X_OFFSET, knobLow.getY() + KNOB_Y_OFFSET, null);
-        //rotate brush back
-        brush.rotate(-angleLow, Knob.WIDTH/2 + knobLow.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobLow.getY() + KNOB_Y_OFFSET);
-    
-        //rotate brush around mid knob by its current angle before painting it        
-        double angleMid = Math.toRadians(knobMid.getAngle());
-        brush.rotate(angleMid, Knob.WIDTH/2 + knobMid.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobMid.getY() + KNOB_Y_OFFSET);
-        brush.drawImage(Knob.getImage(), knobMid.getX() + KNOB_X_OFFSET, knobMid.getY() + KNOB_Y_OFFSET, null);
-        //rotate brush back
-        brush.rotate(-angleMid, Knob.WIDTH/2 + knobMid.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobMid.getY() + KNOB_Y_OFFSET);
-    
-        //rotate brush around high knob by its current angle before painting it        
-        double angleHigh = Math.toRadians(knobHigh.getAngle());
-        brush.rotate(angleHigh, Knob.WIDTH/2 + knobHigh.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobHigh.getY() + KNOB_Y_OFFSET);
-        brush.drawImage(Knob.getImage(), knobHigh.getX() + KNOB_X_OFFSET, knobHigh.getY() + KNOB_Y_OFFSET, null);
-        //rotate brush back
-        brush.rotate(-angleHigh, Knob.WIDTH/2 + knobHigh.getX() + KNOB_X_OFFSET, Knob.HEIGHT/2 + knobHigh.getY() + KNOB_Y_OFFSET);
-        
+        brush.drawImage(playIcon, PLAY_X, PLAY_Y, null);
+        brush.drawImage(pauseIcon, PAUSE_X, PAUSE_Y, null);
+        brush.drawImage(addIcon, ADD_X, ADD_Y, null);
+        brush.drawImage(removeIcon, REMOVE_X, REMOVE_Y, null);
+
     }
     
 
     /**
-     * Add our knobs and their respective mouse listeners
-     * called every repaint()
+     * Add our knobs and their respective mouse listeners (to factor out code)
      */
-    private void addKnobs() {
-    	
+    private void addKnobs() { 	
 		this.add(knobVolume);
-		_mouseDownCoordinates.put(knobVolume.getText(), 0);
-		knobVolume.addMouseListener(new KnobMouseListener(knobVolume));
-		knobVolume.addMouseMotionListener(new KnobMouseMotionListener(knobVolume));
-		
 		this.add(knobSpeed);
-		_mouseDownCoordinates.put(knobSpeed.getText(), 0);
-		knobSpeed.addMouseListener(new KnobMouseListener(knobSpeed));
-		knobSpeed.addMouseMotionListener(new KnobMouseMotionListener(knobSpeed));
-		
 		this.add(knobLow);
-		_mouseDownCoordinates.put(knobLow.getText(), 0);
-		knobLow.addMouseListener(new KnobMouseListener(knobLow));
-		knobLow.addMouseMotionListener(new KnobMouseMotionListener(knobLow));
-		
 		this.add(knobMid);
-		_mouseDownCoordinates.put(knobMid.getText(), 0);
-		knobMid.addMouseListener(new KnobMouseListener(knobMid));
-		knobMid.addMouseMotionListener(new KnobMouseMotionListener(knobMid));
-		
 		this.add(knobHigh);
-		_mouseDownCoordinates.put(knobHigh.getText(), 0);
-		knobHigh.addMouseListener(new KnobMouseListener(knobHigh));
-		knobHigh.addMouseMotionListener(new KnobMouseMotionListener(knobHigh));
     }
     
     
@@ -177,87 +116,17 @@ public class SongPanel extends JPanel {
 			String dir = System.getProperty("user.dir") + "/src/icons/";
 			return ImageIO.read(new File(dir + name + ".png"));
 		} catch (IOException e) {
-			System.out.println("ERROR: IOException when trying to load image in SingPanel.getPlayIcon()");
+			System.out.println("ERROR: IOException when trying to load image in SongPanel.getIcon()");
 			return null;
 		}
 	}
-    
-    
-    /**
-     * 
-     * Private class KnobMouseListener that listens for mouse down events
-     * We store the y-coordinate on this event, which will be used on the
-     * MouseDragged event in our mouse motion listener
-     *
-     */
-	private class KnobMouseListener implements MouseListener {
 
-		private Knob _knob;
-
-		public KnobMouseListener(Knob knob) {
-			_knob = knob;
-		}
-
-		@Override
-		public void mouseClicked(MouseEvent e) {}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			_mouseDownCoordinates.put(_knob.getText(), e.getY());
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent e) {}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {}
-
-		@Override
-		public void mouseExited(MouseEvent e) {}
-	}
-	
-	
-	/**
-	 * Private class that listens for MouseDragged events, which turn the knobs
-	 * @author abok
-	 *
-	 */
-	private class KnobMouseMotionListener implements MouseMotionListener {
-
-		private Knob _knob;
-
-		public KnobMouseMotionListener(Knob knob) {
-			_knob = knob;
-		}
-
-		@Override
-		public void mouseDragged(MouseEvent e) {
-			double rotation = 0.5 * (_mouseDownCoordinates.get(_knob.getText()) - e.getY());
-			_knob.rotateImage(rotation);
-
-			if (_knob.getText().equals("Volume"))
-				SongApp.changeVolume(rotation / 50);
-			else if (_knob.getText().equals("Speed"))
-				SongApp.changeSpeed(rotation * 0.001);
-
-			_mouseDownCoordinates.put(_knob.getText(), e.getY());
-			SongPanel.this.repaint();
-		}
-
-		@Override
-		public void mouseMoved(MouseEvent e) {}
-	}
-	
 	/**
 	 * Private class that listens for Mouse click events on the play/pause button
 	 * @author abok
 	 *
 	 */
 	private class SoundPanelMouseListener implements MouseListener {
-
-		public SoundPanelMouseListener() {
-
-		}
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
