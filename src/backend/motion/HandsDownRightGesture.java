5696f2ac4f7c96eebd1887dace7d168c565afc4b
bdbauer <benjamin_d_bauer@brown.edu>
2014-05-02 14:43:49 -0400

    Fixed up and down gestures to only work when there's a single hand.

--- src/backend/motion/HandsDownRightGesture.java
+++ src/backend/motion/HandsDownRightGesture.java
@@ -10,23 +10,25 @@ public class HandsDownRightGesture {
 		controller.frame().gestures();
 		HandList hands = controller.frame().hands();
 		HandList prevHands = controller.frame(1).hands();
-		
-		if (!hands.isEmpty() && !prevHands.isEmpty()){
-			Hand rightHand = hands.rightmost();
-			Hand prevRight = prevHands.rightmost();
-			
-			float dif = rightHand.stabilizedPalmPosition().getY() - prevRight.stabilizedPalmPosition().getY();
-						
-			int numFingers = rightHand.fingers().count();
 
-			if(dif <= -3 && numFingers >= 3) {
-				if (rightHand.stabilizedPalmPosition().getX() > 80.0){
-					return true;
+		if (!hands.isEmpty() && !prevHands.isEmpty()){
+			if (hands.count() == 1){
+				Hand rightHand = hands.rightmost();
+				Hand prevRight = prevHands.rightmost();
+
+				float dif = rightHand.stabilizedPalmPosition().getY() - prevRight.stabilizedPalmPosition().getY();
+
+				int numFingers = rightHand.fingers().count();
+
+				if(dif <= -3 && numFingers >= 3) {
+					if (rightHand.stabilizedPalmPosition().getX() > 80.0){
+						return true;
+					}
 				}
 			}
 
 		}
 		return false;
 	}
-	
+
 }
