453b26910aae0dd15622908ee4b4dbc14a71d5bb
Arun Varma <arun_varma@brown.edu>
2014-05-05 14:21:22 -0400

    disabled system err

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -12,6 +12,8 @@ import backend.audio.SongApp;
 
 public class Main {
   public static void main(String[] args) {
+    System.err.close();
+
     // set computer to use opengl
 	System.setProperty("sun.java2d.opengl", "true");
 
